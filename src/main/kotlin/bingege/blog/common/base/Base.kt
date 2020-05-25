c00e43e563f7385df4b4c5a98341b72816edd9d7
ywb <yuweibin@boostfield.com>
2020-05-25 14:21:46 +0800

    Add Base

---
+++ src/main/kotlin/bingege/blog/common/base/Base.kt
@@ -0,0 +1,49 @@
+package bingege.blog.common.base
+
+import com.vladmihalcea.hibernate.type.array.IntArrayType
+import com.vladmihalcea.hibernate.type.array.StringArrayType
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType
+import com.vladmihalcea.hibernate.type.json.JsonNodeBinaryType
+import com.vladmihalcea.hibernate.type.json.JsonNodeStringType
+import com.vladmihalcea.hibernate.type.json.JsonStringType
+import org.hibernate.annotations.GenericGenerator
+import org.hibernate.annotations.TypeDef
+import org.hibernate.annotations.TypeDefs
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.io.Serializable
+import java.time.ZonedDateTime
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.EntityListeners
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@TypeDefs(
+    TypeDef(name = "string-array", typeClass = StringArrayType::class),
+    TypeDef(name = "int-array", typeClass = IntArrayType::class),
+    TypeDef(name = "json", typeClass = JsonStringType::class),
+    TypeDef(name = "jsonb", typeClass = JsonBinaryType::class),
+    TypeDef(name = "jsonb-node", typeClass = JsonNodeBinaryType::class),
+    TypeDef(name = "json-node", typeClass = JsonNodeStringType::class)
+)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class Base : Serializable {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false, nullable = false)
+    open var id: UUID = UUID(0, 0)
+
+    @CreatedDate
+    @Column(columnDefinition = "timestamptz")
+    open var createAt: ZonedDateTime = ZonedDateTime.now()
+
+    @LastModifiedDate
+    @Column(columnDefinition = "timestamptz")
+    open var updateAt: ZonedDateTime = ZonedDateTime.now()
+}
