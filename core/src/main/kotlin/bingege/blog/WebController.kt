9adf9641cba762233cf33a8293ef5c2f0c7562fb
ywb <yuweibin@boostfield.com>
2020-05-27 10:15:41 +0800

    Add 首页界面

---
+++ core/src/main/kotlin/bingege/blog/WebController.kt
@@ -0,0 +1,29 @@
+package bingege.blog
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+class WebController {
+
+    @RequestMapping(value = ["index", "/"])
+    fun index(model: Model): String {
+//        map["avatar"] = ""
+//        map["name"] = "nobody"
+//        map["saying"] = "text"
+        model.addAttribute("avatar", "/image/20190815095735993296.jpeg")
+        model.addAttribute("name", "nobody")
+        model.addAttribute("saying", "fuck")
+        return "index"
+    }
+
+
+    @RequestMapping("/test")
+    fun test(model: Model): String {
+        model.addAttribute("welcome", "Happy !")
+        return "test"
+    }
+
+
+}
